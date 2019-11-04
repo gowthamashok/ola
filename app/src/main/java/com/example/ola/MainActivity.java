2b0951e6966a76226b3085e943f10ab52b794e04
admin <gowthamashokumar@gmail.com>
2019-11-04 16:50:34 +0530

    login screen layout finished

--- app/src/main/java/com/example/ola/MainActivity.java
+++ app/src/main/java/com/example/ola/MainActivity.java
@@ -1,13 +1,109 @@
 package com.example.ola;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    private DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new BookRideFrag()).commit();
+        navigationView.setCheckedItem(R.id.navbar_book_ride);
+
+
+        drawerLayout = findViewById(R.id.nav_drawer_layout);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,
+                toolbar, R.string.nav_drawer_open,R.string.nav_drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId())
+        {
+            case R.id.navbar_book_ride:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new BookRideFrag()).commit();
+                break;
+        }
+        switch (menuItem.getItemId())
+        {
+            case R.id.navbar_your_ride:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new YourRideFrag()).commit();
+                break;
+        }
+        switch (menuItem.getItemId())
+        {
+            case R.id.navbar_rate_card:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new RateCardFrag()).commit();
+                break;
+        }
+        switch (menuItem.getItemId())
+        {
+            case R.id.navbar_payment:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new PaymentFrag()).commit();
+                break;
+        }
+        switch (menuItem.getItemId())
+        {
+            case R.id.navbar_refer_earn:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new ReferFrag()).commit();
+                break;
+        }
+        switch (menuItem.getItemId())
+        {
+            case R.id.navbar_support:
+                Intent intent = new Intent(this,Support.class);
+                startActivity(intent);
+
+                break;
+        }
+        switch (menuItem.getItemId())
+        {
+            case R.id.navbar_about:
+
+                Intent intent = new Intent(this,About.class);
+                startActivity(intent);
+
+                break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/ola/YourRideFrag.java
@@ -0,0 +1,18 @@
+package com.example.ola;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class YourRideFrag extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.your_ride_frag,container,false);
+    }
+}
