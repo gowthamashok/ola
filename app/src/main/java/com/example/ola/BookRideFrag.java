2036bd7b69a70557539334ad35ece360b84b07f1
admin <gowthamashokumar@gmail.com>
2019-11-16 15:27:45 +0530

    gps permission added

--- app/src/main/java/com/example/ola/BookRideFrag.java
+++ app/src/main/java/com/example/ola/BookRideFrag.java
@@ -1,13 +1,21 @@
 package com.example.ola;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.Manifest;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -15,6 +23,7 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -26,14 +35,23 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class BookRideFrag extends Fragment implements OnMapReadyCallback {
 
     Fragment fragment;
     private GoogleMap mMap;
-    Location location;
+    Location mlocation;
     FusedLocationProviderClient fusedLocationProviderClient;
     public static final int Request_code = 101;
+    double v;
+    double t;
+
+    public BookRideFrag()
+    {
+
+    }
 
     @Nullable
     @Override
@@ -44,29 +62,97 @@ public class BookRideFrag extends Fragment implements OnMapReadyCallback {
         Toolbar toolbar = v.findViewById(R.id.toolbar);
         getActivity().setTitle("Book your Ride");
 
-      
 
-        SupportMapFragment supportMapFragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.map);
+
+        SupportMapFragment supportMapFragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.google_map);
         supportMapFragment.getMapAsync(this);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient((Activity)getActivity());
+
+
+
+        LocationManager mlocManager = (LocationManager) this.getActivity().getSystemService(Context.LOCATION_SERVICE);
+        boolean enabled = mlocManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if(!enabled) {
+            showDialogGPS();
+        }
+        if (ContextCompat.checkSelfPermission(this.getActivity().getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(getContext(),"no permission",Toast.LENGTH_LONG).show();
+        }
+
+        else {
+            onMapReady(mMap);
+        }
 
         return v;
 
     }
 
+    public void showDialogGPS() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setCancelable(false);
+        builder.setTitle("Enable GPS");
+        builder.setMessage("Please enable GPS");
+        builder.setInverseBackgroundForced(true);
+        builder.setPositiveButton("Enable", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                startActivity(
+                        new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+            }
+        });
+        builder.setNegativeButton("Ignore", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(getContext(),TurnOnGps.class);
+                startActivity(intent);
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
 
+
+
+    private void Getloaction() {
+
+
+
+
+    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        float zoomlevel = 16.0f;
-        LatLng sydney = new LatLng(13.0827, 80.2707);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney,zoomlevel));
+        if (ContextCompat.checkSelfPermission(this.getActivity().getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
 
-//        mMap.setMyLocationEnabled(true);
-//        mMap.setOnMyLocationButtonClickListener((GoogleMap.OnMyLocationButtonClickListener) this);
-//        mMap.setOnMyLocationClickListener((GoogleMap.OnMyLocationClickListener) this);
+            Toast.makeText(getContext(),"no permission",Toast.LENGTH_LONG).show();
+
+        } else {
+            Task<Location>task = fusedLocationProviderClient.getLastLocation();
+            task.addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null)
+                    {
+                        mlocation=location;
+
+                        t = mlocation.getLatitude();
+                        v = mlocation.getLongitude();
+                        float zoomlevel = 16.0f;
+                        LatLng sydney = new LatLng(t,v);
+                        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney,zoomlevel));
+
+                    }
+                }
+            });
+
+        }
+
 
     }
 
